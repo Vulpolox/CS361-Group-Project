eb7eecb2774d917791d26eced8477b13d31aa894
Haley Nilsen <113367887+hmnhpd@users.noreply.github.com>
2025-04-24 11:27:56 -0500

    Delete api/risk_prioritization.js

--- api/risk_prioritization.js
+++
@@ -1,6 +0,0 @@
-// Risk Prioritization Model
-function prioritizeRisks(risks) {
-    return risks.sort((a, b) => (b.likelihood * b.impact) - (a.likelihood * a.impact));
-}
-
-module.exports = { prioritizeRisks };
